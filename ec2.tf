1d8ff022f29e30d8549baa3fb657bcfa4e899fab
TieuLong21Prosper <128500598+TieuLong21Prosper@users.noreply.github.com>
2024-04-04 13:39:03 +0700

    update

--- ec2.tf
+++ ec2.tf
@@ -5,17 +5,6 @@ provider "aws" {
 }
 
 
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
-    key     = "build/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "terraform-user"
-  }
-}
-
-
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
 
